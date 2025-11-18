92217540783fd7aa9f772c8b3c9492bce4e34919
hamad610120 <hamadalmansoor8@gmail.com>
2025-11-18 16:18:43 +0300

    Add files via upload

--- SPS.py
+++ SPS.py
@@ -4,5 +4,8 @@ from supabase import create_client, Client
 url: str = "https://xnyzgnfiqczxlzuocttt.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhueXpnbmZpcWN6eGx6dW9jdHR0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTU3MDYzNiwiZXhwIjoyMDY3MTQ2NjM2fQ.PfbT65vmXaHJZZThp3_05RXuhZo7FZ_1do4y8_WrTeo"
 
-supabase: Client = create_client(url, key)
+def db() -> Client:
+    client = create_client(url, key)
+    return client
+
 print("✅ Supabase API connected successfully")
